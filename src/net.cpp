f206f9a665537d9b328667103339b43538bc2f62
Markcus Langston <kramrok@gmail.com>
2018-02-13 19:07:47 -0600

    Fixes in main.h etc...

--- src/net.cpp
+++ src/net.cpp
@@ -1173,7 +1173,8 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {NULL, NULL}
+    {"vps170017.vps.ovh.ca", "158.69.60.76"}
+	{NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
