a98f274c1dfa43e4623aa964c67d182f18e4f8c5
Markcus Langston <kramrok@gmail.com>
2018-02-13 19:32:12 -0600

    Fix syantax error in net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1173,7 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"vps170017.vps.ovh.ca", "158.69.60.76"}
+	{"vps170017.vps.ovh.ca", "158.69.60.76"},
 	{NULL, NULL}
 };
 
